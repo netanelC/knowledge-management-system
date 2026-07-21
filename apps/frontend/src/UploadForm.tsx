f1166abcdc27a959d5cc44ab37b74a77711c155b
netanelC <netanel153@gmail.com>
2026-07-21 22:09:52 +0300

    fix: restore backend types.ts for frontend to import to fix tsc -b build error

--- apps/frontend/src/UploadForm.tsx
+++ apps/frontend/src/UploadForm.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import type { FormEvent } from 'react';
-import type { Asset } from '../../backend/node_modules/@prisma/client';
+import type { Asset } from '../../backend/src/types';
 
 export const UploadForm: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
