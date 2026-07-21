01d002a34d536ea1e1660abac7bce7dc61f8faef
netanelC <netanel153@gmail.com>
2026-07-21 20:54:38 +0300

    fix: propagate health check error to controller

--- apps/backend/src/components/health/model.ts
+++ apps/backend/src/components/health/model.ts
@@ -1,9 +1,5 @@
 import { pingDatabaseQuery } from './dal';
 
 export const pingDatabase = async (): Promise<number | undefined> => {
-  try {
-    return await pingDatabaseQuery();
-  } catch (error) {
-    return undefined;
-  }
+  return await pingDatabaseQuery();
 };
